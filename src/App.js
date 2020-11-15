37c842ff32af0d7ada65d21d41d92dcbcccf012f
Ben <ben.dalton@madetech.com>
2020-11-15 15:25:51 +0000

    create currentpage state hook, use axios to fetch pokemon names

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import axios from 'axios';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon");
 
+  useEffect(() => {
+    axios.get(currentPageUrl).then(res => {
+      setPokemon(res.data.results.map(p => p.name))
+    })
+  }, [currentPageUrl])
+  
   return (
     <PokemonList pokemon={pokemon} />
   );
