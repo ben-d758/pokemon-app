33036ed85b8a78ea4bbc3ed971cebe03b896dc95
Ben <ben.dalton@madetech.com>
2020-11-15 15:24:51 +0000

    create pokemon state hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PokemonList from './PokemonList';
+import axios from 'axios';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
   return (
-    null
+    <PokemonList pokemon={pokemon} />
   );
 }
 
